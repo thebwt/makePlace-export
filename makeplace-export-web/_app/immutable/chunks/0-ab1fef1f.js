2ac98783d14d3a480270a775c48289881e22512e
Brandon Tomlinson <thebwt@gmail.com>
2024-05-10 22:45:50 +0000

    Forgot some files

---
+++ makeplace-export-web/_app/immutable/chunks/0-ab1fef1f.js
@@ -0,0 +1,2 @@
+import {default as t} from "../components/pages/_layout.svelte-ac6593f6.js";
+export {t as component};
---
+++ makeplace-export-web/_app/immutable/chunks/1-5f84d112.js
@@ -0,0 +1,2 @@
+import {default as t} from "../components/error.svelte-6d287cd9.js";
+export {t as component};
